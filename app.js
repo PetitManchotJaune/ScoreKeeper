96abe7b4788aeb8440fc506cc1882aea829647c4
PetitManchotJaune <csantos.doris@gmail.com>
2021-06-25 11:12:40 +0100

    preventing player one from continuing to add to the score when it hits 5

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const p1Btn = document.querySelector('#p1Btn');
 const p1Display = document.querySelector('#p1Display');
 
 let p1Score = 0;
+let winningScore = 5;
 
 p1Btn.addEventListener('click', () => {
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+    if (p1Score !== winningScore) {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
 })
