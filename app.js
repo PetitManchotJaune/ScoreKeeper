0561ee5d12225e517d338d4dc9415eb7cc6bcd8c
PetitManchotJaune <csantos.doris@gmail.com>
2021-06-25 12:20:59 +0100

    when players select another limit to hit (in better words: winning score) apply the new winning score and reset the game

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const p2Btn = document.querySelector('#p2Btn');
 const resetBtn = document.querySelector('#resetBtn');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -29,10 +30,18 @@ p2Btn.addEventListener('click', () => {
     }
 })
 
-resetBtn.addEventListener('click', () => {
+// (Arrow function don't bind to 'this' keyword)
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetBtn.addEventListener('click', reset);
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-})
+}
