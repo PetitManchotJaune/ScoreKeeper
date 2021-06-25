47fce70a799907bba771fccaa0f63710151a7c14
PetitManchotJaune <csantos.doris@gmail.com>
2021-06-25 11:21:52 +0100

    update display on player two button and preventing player two from continuing to add to the score when it hits 5

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const p1Btn = document.querySelector('#p1Btn');
+const p2Btn = document.querySelector('#p2Btn');
 const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
+let p2Score = 0;
 let winningScore = 5;
 
 p1Btn.addEventListener('click', () => {
     if (p1Score !== winningScore) {
         p1Score += 1;
         p1Display.textContent = p1Score;
+    }
+})
+
+p2Btn.addEventListener('click', () => {
+    if (p2Score !== winningScore) {
+        p2Score += 1;
+        p2Display.textContent = p2Score;
     }
 })
